dec83e7089ee27c4da3ca0607f78b285f41263ea
Anderson Fortaleza <afortaleza@gmail.com>
2012-05-24 16:41:45 -0300

    Add process pages and improve bug details box for emails

--- EmailMonitor.php
+++ EmailMonitor.php
@@ -24,18 +24,58 @@
 
         function display_emails($p_event, $p_bug_id)
         {
+            collapse_open('EmailMonitor');
 ?>
+            <br/>
+            <a name="changesets"/>
             <table class="width100" cellspacing="1">
                 <tr>
                     <td class="form-title">
-                        <?php echo plugin_lang_get('email_list_title') ?>
+                        <?php collapse_icon( 'EmailMonitor' ); echo plugin_lang_get('email_list_title') ?>
                     </td>
                 </tr>
-                <tr>
-                    <td>afortaleza@hotmail.com</td>
+                <tr class="row-1">
+                    <td class="category" width="15%">
+                        <?= plugin_lang_get('email_list') ?>
+                    </td>
+                    <td>
+<?php
+                        for ($i = 0; $i < 4; $i++)
+                        {
+                            $s_delete_link = plugin_page('email_delete').'&bug_id='.$f_bug_id.'&email=afortaleza'.$i.'@vivaimoveis.com.br'.form_security_param('plugin_EmailMonitor_email_delete');
+?>
+                            <a href="mailto:root@localhost">afortaleza<?= echo $i ?>@vivaimoveis.com.br</a> 
+                            [<a class="small" href="<?= $s_delete_link ?>">
+                                    <?= plugin_lang_get('email_delete') ?>
+                            </a>]
+<?php
+                        }
+?>
+                        <br/>
+                        <br/>
+
+                        <?php echo plugin_lang_get('email') ?>
+                        <form action="<?= plugin_page('email_add') ?>" method="post">
+                            <input type="hidden" name="bug_id" value="<?= $f_bug_id ?>">
+                            <input type="text" name="email" style="width: 180px;">
+                            <input type="submit" class="button" value="<?= plugin_lang_get('email_add') ?>">
+                        </form>
+                    </td>
                 </tr>
             </table>
 <?php
+            collapse_closed('EmailMonitor');
+?>
+            <br/>
+            <table class="width100" cellspacing="1">
+                <tr>
+                    <td class="form-title">
+                        <?php collapse_icon('EmailMonitor'); echo plugin_lang_get('email_list_title') ?>
+                    </td>
+                </tr>
+            </table>
+<?php
+            collapse_end('EmailMonitor');
         }
     }
 ?>
---
+++ pages/email_delete.php
@@ -0,0 +1,3 @@
+<?php
+    echo "Delete email not implemented";
+?>
