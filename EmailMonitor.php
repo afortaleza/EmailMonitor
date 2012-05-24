feda3715e9de4660926a68faabc61f224ceb4faa
Anderson Fortaleza <afortaleza@gmail.com>
2012-05-24 16:58:20 -0300

    Fix query string variable name for bug id

--- EmailMonitor.php
+++ EmailMonitor.php
@@ -24,7 +24,7 @@
 
         function display_emails($p_event, $p_bug_id)
         {
-            $f_bug_id = gpc_get_int( 'bug_id' );
+            $f_bug_id = gpc_get_int('id');
             collapse_open('EmailMonitor');
 ?>
             <br/>
