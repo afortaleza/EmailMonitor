fc306cfad9eb65ade084ea463ded9bbee2d8c605
Anderson Fortaleza <afortaleza@gmail.com>
2012-05-25 06:51:32 -0300

    Adding API functions

---
+++ EmailMonitor.API.php
@@ -0,0 +1,45 @@
+<?php
+
+# Copyright (c) 2012 Anderson Fortaleza
+# Licensed under the MIT license
+
+/**
+ * Retrieves a list of emails given the bug_id
+ * @param int Bug ID
+ * @return array Emails associated to the bug
+ */
+function EmailMonitor_List($t_bug_id)
+{
+    $t_emails = array();
+    $t_email_table = plugin_table('email', 'EmailMonitor');
+    $t_query = "SELECT email FROM $t_email_table WHERE bug_id = $t_bug_id";
+
+    $t_result = db_query($t_query);
+
+    while($t_row = db_fetch_array($t_result))
+    {
+        array_push($t_emails, $t_row['email']);
+    }
+
+    return $t_emails;
+}
+
+function EmailMonitor_Add($t_bug_id, $t_email)
+{
+    $t_email_table = plugin_table('email', 'EmailMonitor');
+    $t_query = "INSERT INTO $t_email_table VALUES (".db_param().", ".db_param().")";
+
+    db_query_bound($t_query, array($t_bug_id, $t_email));
+
+    return db_insert_id($t_email_table);
+}
+
+function EmailMonitor_Delete($t_id)
+{
+    $t_email_table = plugin_table('email', 'EmailMonitor');
+    $t_query = "DELETE FROM $t_email_table WHERE id = $t_id";
+
+    db_query_bound($t_query);
+}
+
+?>
--- EmailMonitor.php
+++ EmailMonitor.php
@@ -1,96 +1,101 @@
 <?php
-    final class EmailMonitorPlugin extends MantisPlugin {
-        function register() {
-            $this->name = 'Email Monitor';
-            $this->description = 'Allows email subscriptions to the issue monitor feature.';
-            $this->page = '';
 
-            $this->version = '1.0';
-            $this->requires = array(
-                'MantisCore' => '1.2.0',
-            );
+# Copyright (c) 2012 Anderson Fortaleza
+# Licensed under the MIT license
 
-            $this->author = 'Anderson Fortaleza';
-            $this->contact = 'afortaleza@hotmail.com';
-            $this->url = 'https://www.github.com/afortaleza';
-        }
+final class EmailMonitorPlugin extends MantisPlugin {
+    function register() {
+        $this->name = 'Email Monitor';
+        $this->description = 'Allows email subscriptions to the issue monitor feature.';
+        $this->page = '';
 
-        function hooks()
-        {
-            return array(
-                'EVENT_VIEW_BUG_EXTRA' => 'display_emails',
-            );
-        }
+        $this->version = '1.0';
+        $this->requires = array(
+            'MantisCore' => '1.2.0',
+        );
 
-        function schema()
-        {
-            return array(
-                array('CreateTableSQL', 
-                	array(
-                		plugin_table('email'), 
-                		"
-                    	id			I		NOTNULL UNSIGNED AUTOINCREMENT PRIMARY,
-                    	bug_id		I		NOTNULL UNSIGNED,
-                    	email		C(128)	NOTNULL
-                    	"
-            		)
-        		),
-            );
-        }
-
-        function display_emails($p_event, $p_bug_id)
-        {
-            $f_bug_id = gpc_get_int('id');
-            collapse_open('EmailMonitor');
-?>
-            <br/>
-            <a name="changesets"/>
-            <table class="width100" cellspacing="1">
-                <tr>
-                    <td class="form-title" colspan="2">
-                        <?php collapse_icon( 'EmailMonitor' ); echo plugin_lang_get('email_list_title') ?>
-                    </td>
-                </tr>
-                <tr class="row-1">
-                    <td class="category" width="15%">
-                        <?php echo plugin_lang_get('email_list') ?>
-                    </td>
-                    <td>
-<?php
-                        for ($i = 0; $i < 4; $i++)
-                        {
-                            $s_delete_link = plugin_page('email_delete').'&bug_id='.$f_bug_id.'&email=afortaleza'.$i.'@vivaimoveis.com.br'.form_security_param('plugin_EmailMonitor_email_delete');
-?>
-                            <a href="mailto:root@localhost">afortaleza<?php echo $i ?>@vivaimoveis.com.br</a> 
-                            [<a class="small" href="<?php echo $s_delete_link ?>"><?php echo plugin_lang_get('email_delete') ?></a>]
-<?php
-                        }
-?>
-                        <br/>
-                        <br/>
-
-                        <?php echo plugin_lang_get('email') ?>
-                        <form action="<?php echo plugin_page('email_add') ?>" method="post">
-                            <input type="hidden" name="bug_id" value="<?php echo $f_bug_id ?>">
-                            <input type="text" name="email" style="width: 180px;">
-                            <input type="submit" class="button" value="<?php echo plugin_lang_get('email_add') ?>">
-                        </form>
-                    </td>
-                </tr>
-            </table>
-<?php
-            collapse_closed('EmailMonitor');
-?>
-            <br/>
-            <table class="width100" cellspacing="1">
-                <tr>
-                    <td class="form-title">
-                        <?php collapse_icon('EmailMonitor'); echo plugin_lang_get('email_list_title') ?>
-                    </td>
-                </tr>
-            </table>
-<?php
-            collapse_end('EmailMonitor');
-        }
+        $this->author = 'Anderson Fortaleza';
+        $this->contact = 'afortaleza@hotmail.com';
+        $this->url = 'https://www.github.com/afortaleza';
     }
+
+    function hooks()
+    {
+        return array(
+            'EVENT_VIEW_BUG_EXTRA' => 'display_emails',
+        );
+    }
+
+    function schema()
+    {
+        return array(
+            array('CreateTableSQL', 
+            	array(
+            		plugin_table('email'), 
+            		"
+                	id			I		NOTNULL UNSIGNED AUTOINCREMENT PRIMARY,
+                	bug_id		I		NOTNULL UNSIGNED,
+                	email		C(128)	NOTNULL
+                	"
+        		)
+    		),
+        );
+    }
+
+    function display_emails($p_event, $p_bug_id)
+    {
+        $f_bug_id = gpc_get_int('id');
+        collapse_open('EmailMonitor');
+?>
+        <br/>
+        <a name="changesets"/>
+        <table class="width100" cellspacing="1">
+            <tr>
+                <td class="form-title" colspan="2">
+                    <?php collapse_icon( 'EmailMonitor' ); echo plugin_lang_get('email_list_title') ?>
+                </td>
+            </tr>
+            <tr class="row-1">
+                <td class="category" width="15%">
+                    <?php echo plugin_lang_get('email_list') ?>
+                </td>
+                <td>
+<?php
+                    for ($i = 0; $i < 4; $i++)
+                    {
+                        $s_delete_link = plugin_page('email_delete').'&bug_id='.$f_bug_id.'&email=afortaleza'.$i.'@vivaimoveis.com.br'.form_security_param('plugin_EmailMonitor_email_delete');
+?>
+                        <a href="mailto:root@localhost">afortaleza<?php echo $i ?>@vivaimoveis.com.br</a> 
+                        [<a class="small" href="<?php echo $s_delete_link ?>"><?php echo plugin_lang_get('email_delete') ?></a>]
+<?php
+                    }
+?>
+                    <br/>
+                    <br/>
+
+                    <?php echo plugin_lang_get('email') ?>
+                    <form action="<?php echo plugin_page('email_add') ?>" method="post">
+                        <input type="hidden" name="bug_id" value="<?php echo $f_bug_id ?>">
+                        <input type="text" name="email" style="width: 180px;">
+                        <input type="submit" class="button" value="<?php echo plugin_lang_get('email_add') ?>">
+                    </form>
+                </td>
+            </tr>
+        </table>
+<?php
+        collapse_closed('EmailMonitor');
+?>
+        <br/>
+        <table class="width100" cellspacing="1">
+            <tr>
+                <td class="form-title">
+                    <?php collapse_icon('EmailMonitor'); echo plugin_lang_get('email_list_title') ?>
+                </td>
+            </tr>
+        </table>
+<?php
+        collapse_end('EmailMonitor');
+    }
+}
+
 ?>
