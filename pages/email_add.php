e5f4ba8521e2250c4db7e0d345db06c7d7e7ea40
Anderson Fortaleza <afortaleza@gmail.com>
2012-05-25 09:31:16 -0300

    Add code to add emails to the monitoring list

--- pages/email_add.php
+++ pages/email_add.php
@@ -1,3 +1,25 @@
 <?php
-    echo "Add email not implemented";
+
+# Copyright (c) 2012 Anderson Fortaleza
+# Licensed under the MIT license
+
+form_security_validate('plugin_EmailMonitor_email_add');
+require_once(config_get('plugin_path').'EmailMonitor/EmailMonitor.API.php');
+
+$t_bug_id = $_POST['bug_id'];
+$t_email = $_POST['email'];
+
+if (email_is_valid($t_email))
+{
+	EmailMonitor_Add($t_bug_id, $t_email);
+	
+}
+else 
+{
+	trigger_error(ERROR_EMAIL_INVALID);
+}
+
+form_security_purge('plugin_EmailMonitor_email_add');
+print_successful_redirect_to_bug($t_bug_id);
+
 ?>
